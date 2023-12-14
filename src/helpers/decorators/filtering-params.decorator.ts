2660d1bb840fc3c8de044b03340851a30a69b537
eloygm92 <eloygm.92@uma.es>
2023-12-14 13:24:15 +0100

    Fix(decorators): fix filters, paginator and sorting

--- src/helpers/decorators/filtering-params.decorator.ts
+++ src/helpers/decorators/filtering-params.decorator.ts
@@ -1,5 +1,9 @@
-import { BadRequestException, createParamDecorator, ExecutionContext } from "@nestjs/common";
-import { Request } from "express";
+import {
+  BadRequestException,
+  createParamDecorator,
+  ExecutionContext,
+} from '@nestjs/common';
+import { Request } from 'express';
 
 export interface Filtering {
   property: string;
@@ -23,27 +27,38 @@ export enum FilterRule {
   IS_NOT_NULL = 'isnotnull',
 }
 
-export const FilteringParams = createParamDecorator((data, ctx: ExecutionContext): Filtering[] => {
-  const req: Request = ctx.switchToHttp().getRequest();
-  const filter = req.query.filter as string[];
-  if (!filter) return null;
+export const FilteringParams = createParamDecorator(
+  (data, ctx: ExecutionContext): Filtering[] => {
+    const req: Request = ctx.switchToHttp().getRequest();
+    let filter = req.query.filter as string[];
+    if (!filter) return null;
+    if (typeof filter != 'object') filter = [filter];
 
-  // check if the valid params sent is an array
-  if (typeof data != 'object') throw new BadRequestException('Invalid filter parameter');
-
-  const fitlerArray: {property: string, rule: string, value: string}[] = []
-  // validate the format of the filter, if the rule is 'isnull' or 'isnotnull' it don't need to have a value
-  filter.forEach((filter) => {
-    if (!filter.match(/^[a-zA-Z0-9_]+:(eq|neq|gt|gte|lt|lte|like|nlike|in|nin):[a-zA-Z0-9_,]+$/) && !filter.match(/^[a-zA-Z0-9_]+:(isnull|isnotnull)$/)) {
+    // check if the valid params sent is an array
+    if (typeof data != 'object')
       throw new BadRequestException('Invalid filter parameter');
-    }
-    // extract the parameters and validate if the rule and the property are valid
-    const [property, rule, value] = filter.split(':');
-    if (!data.includes(property)) throw new BadRequestException(`Invalid filter property: ${property}`);
-    if (!Object.values(FilterRule).includes(rule as FilterRule)) throw new BadRequestException(`Invalid filter rule: ${rule}`);
-    fitlerArray.push({ property, rule, value })
-  });
 
-  return fitlerArray;
-  //return { property, rule, value };
-});
+    const fitlerArray: { property: string; rule: string; value: string }[] = [];
+    // validate the format of the filter, if the rule is 'isnull' or 'isnotnull' it don't need to have a value
+    filter.forEach((filter) => {
+      if (
+        !filter.match(
+          /^[a-zA-Z0-9_\.]+:(eq|neq|gt|gte|lt|lte|like|nlike|in|nin):[a-zA-Z0-9_,]+$/,
+        ) &&
+        !filter.match(/^[a-zA-Z0-9_\.]+:(isnull|isnotnull)$/)
+      ) {
+        throw new BadRequestException('Invalid filter parameter');
+      }
+      // extract the parameters and validate if the rule and the property are valid
+      const [property, rule, value] = filter.split(':');
+      if (!data.includes(property))
+        throw new BadRequestException(`Invalid filter property: ${property}`);
+      if (!Object.values(FilterRule).includes(rule as FilterRule))
+        throw new BadRequestException(`Invalid filter rule: ${rule}`);
+      fitlerArray.push({ property, rule, value });
+    });
+
+    return fitlerArray;
+    //return { property, rule, value };
+  },
+);
--- src/helpers/typeOrm.helper.ts
+++ src/helpers/typeOrm.helper.ts
@@ -9,18 +9,78 @@ export const getWhere = (filter: Filtering[]) => {
   const where = [];
 
   filter.forEach((filter) => {
-    if (filter.rule == FilterRule.IS_NULL) where.push({ [filter.property]: IsNull() });
-    if (filter.rule == FilterRule.IS_NOT_NULL) where.push({ [filter.property]: Not(IsNull()) });
-    if (filter.rule == FilterRule.EQUALS) where.push({ [filter.property]: filter.value });
-    if (filter.rule == FilterRule.NOT_EQUALS) where.push({ [filter.property]: Not(filter.value) });
-    if (filter.rule == FilterRule.GREATER_THAN) where.push({ [filter.property]: MoreThan(filter.value) });
-    if (filter.rule == FilterRule.GREATER_THAN_OR_EQUALS) where.push({ [filter.property]: MoreThanOrEqual(filter.value) });
-    if (filter.rule == FilterRule.LESS_THAN) where.push({ [filter.property]: LessThan(filter.value) });
-    if (filter.rule == FilterRule.LESS_THAN_OR_EQUALS) where.push({ [filter.property]: LessThanOrEqual(filter.value) });
-    if (filter.rule == FilterRule.LIKE) where.push({ [filter.property]: ILike(`%${filter.value}%`) });
-    if (filter.rule == FilterRule.NOT_LIKE) where.push({ [filter.property]: Not(ILike(`%${filter.value}%`)) });
-    if (filter.rule == FilterRule.IN) where.push({ [filter.property]: In(filter.value.split(',')) });
-    if (filter.rule == FilterRule.NOT_IN) where.push({ [filter.property]: Not(In(filter.value.split(','))) });
+    if (filter.rule == FilterRule.IS_NULL) {
+      if (filter.property.includes('.')) {
+        const [entity, property] = filter.property.split('.');
+        where.push({ [entity]: { [property]: IsNull() } });
+      } else where.push({ [filter.property]: IsNull() });
+    }
+    if (filter.rule == FilterRule.IS_NOT_NULL) {
+      if (filter.property.includes('.')) {
+        const [entity, property] = filter.property.split('.');
+        where.push({ [entity]: { [property]: Not(IsNull()) } });
+      } else where.push({ [filter.property]: Not(IsNull()) });
+    }
+    if (filter.rule == FilterRule.EQUALS) {
+      if (filter.property.includes('.')) {
+        const [entity, property] = filter.property.split('.');
+        where.push({ [entity]: { [property]: filter.value } });
+      } else where.push({ [filter.property]: filter.value });
+    }
+    if (filter.rule == FilterRule.NOT_EQUALS) {
+      if (filter.property.includes('.')) {
+        const [entity, property] = filter.property.split('.');
+        where.push({ [entity]: { [property]: Not(filter.value) } });
+      } else where.push({ [filter.property]: Not(filter.value) });
+    }
+    if (filter.rule == FilterRule.GREATER_THAN) {
+      if (filter.property.includes('.')) {
+        const [entity, property] = filter.property.split('.');
+        where.push({ [entity]: { [property]: MoreThan(filter.value) } });
+      } else where.push({ [filter.property]: MoreThan(filter.value) });
+    }
+    if (filter.rule == FilterRule.GREATER_THAN_OR_EQUALS) {
+      if (filter.property.includes('.')) {
+        const [entity, property] = filter.property.split('.');
+        where.push({ [entity]: { [property]: MoreThanOrEqual(filter.value) } });
+      } else where.push({ [filter.property]: MoreThanOrEqual(filter.value) });
+    }
+    if (filter.rule == FilterRule.LESS_THAN) {
+      if (filter.property.includes('.')) {
+        const [entity, property] = filter.property.split('.');
+        where.push({ [entity]: { [property]: LessThan(filter.value) } });
+      } else where.push({ [filter.property]: LessThan(filter.value) });
+    }
+    if (filter.rule == FilterRule.LESS_THAN_OR_EQUALS) {
+      if (filter.property.includes('.')) {
+        const [entity, property] = filter.property.split('.');
+        where.push({ [entity]: { [property]: LessThanOrEqual(filter.value) } });
+      }else where.push({ [filter.property]: LessThanOrEqual(filter.value) });
+    }
+    if (filter.rule == FilterRule.LIKE) {
+      if (filter.property.includes('.')) {
+        const [entity, property] = filter.property.split('.');
+        where.push({ [entity]: { [property]: ILike(`%${filter.value}%`) } });
+      } else where.push({ [filter.property]: ILike(`%${filter.value}%`) });
+    }
+    if (filter.rule == FilterRule.NOT_LIKE) {
+      if (filter.property.includes('.')) {
+        const [entity, property] = filter.property.split('.');
+        where.push({ [entity]: { [property]: Not(ILike(`%${filter.value}%`)) } });
+      } else where.push({ [filter.property]: Not(ILike(`%${filter.value}%`)) });
+    }
+    if (filter.rule == FilterRule.IN) {
+      if (filter.property.includes('.')) {
+        const [entity, property] = filter.property.split('.');
+        where.push({ [entity]: { [property]: In(filter.value.split(",")) } });
+      } else where.push({ [filter.property]: In(filter.value.split(",")) });
+    }
+    if (filter.rule == FilterRule.NOT_IN) {
+      if (filter.property.includes('.')) {
+        const [entity, property] = filter.property.split('.');
+        where.push({ [entity]: { [property]: Not(In(filter.value.split(","))) } });
+      } else where.push({ [filter.property]: Not(In(filter.value.split(","))) });
+    }
   });
   return where;
 }
