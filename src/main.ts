052741742b60698b91f6da52bb0c3428a5b2c0d9
eloygm92 <eloygm.92@uma.es>
2024-01-26 14:38:49 +0100

    Fix: CORS

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
   app.use(cookieParser());
   await app.listen(process.env.APP_PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
